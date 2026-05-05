fb8f816c9c454d9b2ede1a68a698c568df2f7464
Thato Will Joseph <programergeek@gmail.com>
2026-05-05 20:08:53 +0200

    create project flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: 
+    flake-utils.lib.eachDefaultSystem(system: 
+	let
+	    pkgs = nixpkgs.legacyPackages.${system};
+
+	in {
+	    devShells.default = pkgs.mkShell {
+		buildInputs = with pkgs; [
+		    nodejs_22
+		    pnpm
+		    docker
+		];
+	    shellHook = '' 
+		echo "Node: $(node -v)"
+		echo "Pnpm: $(pnpm -v)"
+		echo "Docker: $(docker --version)"
+		'';
+	    };
+	}
+    );
+}
