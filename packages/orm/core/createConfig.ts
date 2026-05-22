c3cd52e8dde337240dd62f9c4cacb73d113fe629
Thato Will Joseph <programergeek@gmail.com>
2026-05-22 02:45:06 +0200

    simplify config function

--- packages/orm/core/createConfig.ts
+++ packages/orm/core/createConfig.ts
@@ -1,74 +1,12 @@
-import pg, { type ClientConfig, PoolConfig } from "pg";
+interface WormDatabaseClientPlugin {}
 
 export interface WormConfigOptions {
-  returnErrorAsValue?: boolean;
-  enablePool?: boolean;
-  poolOptions?: Omit<
-    PoolConfig,
-    "user" | "password" | "host" | "port" | "database"
-  >;
-  clientOptions?: Omit<
-    ClientConfig,
-    "user" | "password" | "host" | "port" | "database"
-  >;
-  schema: string;
-  query: string;
+  client: WormDatabaseClientPlugin;
+  schema?: string;
+  query?: string;
+  out?: string;
 }
 
-interface PostgreSQLAuthOptions {
-  user: string;
-  password: string;
-  host: string;
-  database: string;
-  port: number;
-}
-
-const DEFAULT_CONFIG: WormConfigOptions = {
-  returnErrorAsValue: false,
-  enablePool: true,
-  schema: "./schema.sql", // the database schema will be defined within a single sql file
-  query: "./query", // this points to the directory containing the database queries
-};
-
-export default async function createConfig(
-  db: string | PostgreSQLAuthOptions,
-  options: WormConfigOptions,
-) {
-  const connectionString = (() => {
-    if (typeof db === "string") {
-      return db;
-    }
-
-    return `postgresql://${db.user}:${db.password}@${db.host}:${db.port}/${db.database}`;
-  })();
-
-  const { Client, Pool } = pg;
-
-  const configOptions = {
-    ...DEFAULT_CONFIG,
-    ...options,
-  };
-
-  const client = (() => {
-    if (options.enablePool) {
-      return new Pool({
-        connectionString,
-        ...configOptions.poolOptions,
-      });
-    }
-
-    return new Client({
-      connectionString,
-      ...configOptions.clientOptions,
-    });
-  })();
-
-  return {
-    client,
-    compilerOptions: {
-      returnErrorAsValue: options.returnErrorAsValue ?? false,
-      schemaPath: configOptions.schema,
-      queryPath: configOptions.query,
-    },
-  };
+export default async function createConfig(options: WormConfigOptions) {
+  return { ...options };
 }
